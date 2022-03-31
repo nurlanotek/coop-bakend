e44a5ddfbf75b803494d47737a6eb3e6987b7e93
arirallana <arirallana@hotmail.com>
2022-03-31 11:46:31 +0600

    jobs fetch from database, implement search bar for jobs

--- job/urls.py
+++ job/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import  views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.home, name='home'),
-    path('job',views.job, name='job'),
+    path('job',views.showjobs, name='job'),
+    path('search',views.search, name='search'),
     path('profile_page/', include('profile_page.urls')),
     path('postjob/', include('postjob.urls')),
-    path('logout', views.logout_user, name='logout')
-]
+    path('logout', views.logout_user, name='logout'),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- job/views.py
+++ job/views.py
@@ -1,17 +1,26 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.template import RequestContext
 from django.contrib.auth import logout
 from app1.models import Job
 
 # Create your views here.
-def job(request):
-    return render(request, 'job.html')
-
 def home(request):
     return render(request, 'home.html')
 
 def logout_user(request):
     logout(request)
     return render(request, 'home.html')
+
+def showjobs(request):
+    jobs = Job.objects
+    return render(request, 'job.html', { 'jobs':jobs } )
+
+def search(request):
+    query = request.GET.get('q')
+    print(query)
+    results = Job.objects.filter(Q(title__contains=query) | Q(description__contains=query) | Q(payment__contains=query))
+    print(results)
+    return render(request, 'job.html', { 'jobs':results })
 
 
